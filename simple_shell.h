b552bd677d875972572f9a56e7265441b4aa6740
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-15 23:13:21 +0100

    Adding simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -1,5 +1,14 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
+#include <stdbool.h>
+
+/* String functions Prototypes */
+int _strlen(char *s);
+
+/* main functions Prototypes*/
+bool display_prompt(void);
+int eof_handler(char *commandptr);
+void sigint_handler(int signum __attribute__((unused)));
 
 #endif /* SIMPLE_SHELL_H */
