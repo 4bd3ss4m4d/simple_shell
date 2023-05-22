35ed631360dd0bcabb3296a09c1b360e2b298c70
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-05-22 14:48:44 -0700

    Fixing bugs in the prompt utility functions

--- prompt_utils.c
+++ prompt_utils.c
@@ -43,3 +43,73 @@ void print_num_pch(int n)
 
 	_putchar(x % 10 + '0');
 }
+
+/**
+ * print_ech - runs the echo command
+ * @command: pointer to the pointer that contains the command
+ *
+ * Return: 0 Success or -1 if it fails
+ */
+int print_ech(char **command)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve("/bin/echo", command, environ) == -1)
+		{
+			return (-1);
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
+}
+
+/**
+ * disp_hist - Displays history to the terminal
+ * @c: the pointer to the pointer that contains the command
+ * @s: The status number
+ *
+ * Return: 0 Success or -1 if it fails
+ */
+int disp_hist(__attribute__((unused)) char **c, __attribute__((unused)) int s)
+{
+	char *filename = ".simple_shell_history";
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	int counter = 0;
+	char *er;
+
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+	while ((getline(&line, &len, fp)) != -1)
+	{
+		counter++;
+		er = _itoa(counter);
+		PRINTER(er);
+		free(er);
+		PRINTER(" ");
+		PRINTER(line);
+	}
+	if (line)
+		free(line);
+	fclose(fp);
+	return (0);
+}
+
