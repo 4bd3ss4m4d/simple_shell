6871746dbdf7849a3a91ac597e538579cd0d5a5f
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-05-22 04:17:39 -0700

    String functions 2

--- string_utils2.c
+++ string_utils2.c
@@ -48,3 +48,69 @@ int _isalpha(int c)
 		return (0);
 	}
 }
+
+/**
+ * _itoa - Convert Integer To Char
+ * @n: Int To Convert
+ * Return: Char Pointer
+ */
+char *_itoa(unsigned int n)
+{
+	int len = 0, i = 0;
+	char *s;
+
+	len = int_length(n);
+	s = malloc(len + 1);
+	if (!s)
+		return (NULL);
+	*s = '\0';
+	while (n / 10)
+	{
+		s[i] = (n % 10) + '0';
+		n /= 10;
+		i++;
+	}
+	s[i] = (n % 10) + '0';
+	array_rev(s, len);
+	s[i + 1] = '\0';
+	return (s);
+}
+
+/**
+ *  array_rev - Reverse Array
+ * @arr: Array To Reverse
+ * @len: Length Of Array
+ *
+ * Return: Void(Reverse Array)
+ */
+void array_rev(char *arr, int len)
+{
+	int i;
+	char tmp;
+
+	for (i = 0; i < (len / 2); i++)
+	{
+		tmp = arr[i];
+		arr[i] = arr[(len - 1) - i];
+		arr[(len - 1) - i] = tmp;
+	}
+}
+
+/**
+ * int_length - Determine Length Of Int
+ * @n: Given Int
+ *
+ * Return: Length Of Int
+ */
+int int_length(int n)
+{
+	int len = 0;
+
+	while (n != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
